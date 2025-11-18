0af701502f7aef654da7b4545aa2b3fb8080bc8a
Remy Zane <remyzane@icloud.com>
2025-11-18 18:08:33 +0800

    🌍 部署：优化打包和发布命令

--- publish.sh
+++ publish.sh
@@ -1,14 +1,16 @@
 
-echo "「中文代码补全」插件发布"
+echo "「中文代码补全」插件打包、发布"
 echo "=========================="
 
-echo "请输入 VSCode 令牌："
+echo "请输入 VSCode 令牌（如果只打包不发布，则直接回车）："
 read token
 
 vsce publish --no-dependencies -p $token
 
-
-# vsce package --no-dependencies
-# echo "请输入插件版本（如：1.0.0）："
-# read version
-# vsce publish --packagePath ./vscode-zh-completion-$version.vsix -p $token
+if [ -z "$token" ]; then
+  # 如果 token 为空，则只打包，不发布
+  vsce package --no-dependencies
+else
+  # 如果 token 不为空，则打包 + 发布
+  vsce publish --no-dependencies -p $token
+fi
